4d81047e50e01326ad6376d341efd2f4282009ca
zziro12 <opers7@naver.com>
2020-09-05 00:28:20 +0900

    use array
    todo : use dictionary

---
+++ 모의고사.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+import Foundation
+
+//array를 이용한풀이
+func solution(_ answers:[Int]) -> [Int] {
+    let person1 : [Int] = [1,2,3,4,5]
+    let person2 : [Int] = [2,1,2,3,2,4,2,5]
+    let person3 : [Int] = [3,3,1,1,2,2,4,4,5,5]
+
+    var answer1 : Int = 0
+    var answer2 : Int = 0
+    var answer3 : Int = 0
+    
+    var result : [Int] = []
+    
+    for i in 0..<answers.count{
+        if(person1[i % person1.count] == answers[i]){ answer1 = answer1 + 1 }
+        if(person2[i % person2.count] == answers[i]){ answer2 = answer2 + 1 }
+        if(person3[i % person3.count] == answers[i]){ answer3 = answer3 + 1 }
+    }
+    
+    let findMax = max(max(answer1, answer2), answer3)
+    
+    if findMax == answer1 { result.append(1) }
+    if findMax == answer2 { result.append(2) }
+    if findMax == answer3 { result.append(3) }
+    
+    if result.count >= 2 {
+        result.sorted(by: <)
+    }
+    
+    return result
+}
+
+
+let ex = [3,3,1,1,2,2,4,4,5,5]
+solution(ex)
+
+//dictionary를 이용한풀이
